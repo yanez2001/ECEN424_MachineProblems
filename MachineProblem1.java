4b1c780a7cc6140fe1786de3b0c25ef01c8d9ad6
yanez2001 <156946529+yanez2001@users.noreply.github.com>
2024-09-11 15:03:48 -0500

    Add files via upload
    This is the first commit of the file - yanez

---
+++ MachineProblem1.java
@@ -0,0 +1,38 @@
+/* Yanez Saucedo
+ * Machine Problem #1 - ECEN 424
+ * Texas A&M University - Fall 2024
+ * 
+ * 
+ * 
+ * Implement a calculator that performs the operations of addition,
+ * subtraction and multiplication.
+ * 
+ */
+
+
+// create a class called calculator
+public class calculator {
+
+    private String name;
+
+    public Float addition(Float A, Float B){
+        return A + B;
+    }
+    public Float subtraction(Float A, Float B){
+        return A - B;
+    }
+    public Float multiplication(Float A, Float B){
+        return A * B;
+    }
+    public void setname(String N){
+        return N;
+    }
+    public String getname(){
+
+    }
+}
+
+public class void main(String[] args) {
+    setname("Group 35");
+
+}
