749a73d489088d369c7f504f4378d9b06d718a77
Evan-Tsoi <evantsoi0105@tamu.edu>
2024-09-11 22:23:16 -0500

    Update MachineProblem1.java

--- MachineProblem1.java
+++ MachineProblem1.java
@@ -35,5 +35,49 @@ public class Calculator {
 
 public class void main(String[] args) {
     setname("Group 35");
+    System.out.println("Welcome to the Calculator designed by \"" + mycalc.getName() + "\".");
+
+    while (true) {
+        System.out.println("Enter A to Add, S to Subtract, M to Multiply, and Q to quit.");
+        String command = scanner.nextLine().toUpperCase();
+        
+        if (command.equals("Q")) {
+            System.out.println("Goodbye!");
+            break;
+        }
+
+        if (command.equals("A") || command.equals("S") || command.equals("M")) {
+            System.out.print("Enter argument 1: ");
+            Float arg1 = Float.parseFloat(scanner.nextLine());
+            
+            System.out.print("Enter argument 2: ");
+            Float arg2 = Float.parseFloat(scanner.nextLine());
+
+            Float result = null;
+            switch (command) {
+                case "A":
+                    result = mycalc.addition(arg1, arg2);
+                    System.out.println("The sum of " + arg1 + " and " + arg2 + " is " + result);
+                    break;
+                case "S":
+                    result = mycalc.subtraction(arg1, arg2);
+                    System.out.println("The difference of " + arg1 + " and " + arg2 + " is " + result);
+                    break;
+                case "M":
+                    result = mycalc.multiplication(arg1, arg2);
+                    System.out.println("The product of " + arg1 + " and " + arg2 + " is " + result);
+                    break;
+            }
+        } else {
+            // Invalid command, display welcome message again
+            System.out.println("Invalid input. Please try again.");
+        }
+    }
+
+}
+
+
+public class void main(String[] args) {
+    setname("Group 35");
 
 }
